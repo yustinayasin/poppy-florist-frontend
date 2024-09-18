2b209a6238a5a9c443ac4fcad506048f88e9c90e
yustinayasin <yustinayasin@gmail.com>
2024-09-18 09:48:50 +0700

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "poppy-florist.yustinayasin.com",
-        pathname: "/**", // Allowing all images from this domain
+        protocol: "http",
+        hostname: "139.99.96.99",
+        port: "9000",
+        pathname: "/florist/products/**",
       },
     ],
   },
